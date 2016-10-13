b90b1247bfcae0bf329fbdc08c86e23ef77abadf
Leksha Ramdenee <leksha.ramdenee@tulip.io>
2016-10-13 10:03:49 -0400

    Added a navController as the rootViewController and created a BNRDetailViewController to be added as a view on the navController

---
+++ Homepwner/BNRDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRDetailViewController.h
+//  Homepwner
+//
+//  Created by Leksha Ramdenee on 2016-10-13.
+//  Copyright © 2016 Leksha Ramdenee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRDetailViewController : UIViewController
+
+@end
