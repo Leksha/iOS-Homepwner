680a3e0d0d20666ad5578e312d3f1d8b37a9b46d
Leksha Ramdenee <leksha.ramdenee@tulip.io>
2016-10-31 16:43:00 -0400

    Added BNRItemCell class to add as a view to the UITableViewCell class

---
+++ Homepwner/BNRItemCell.h
@@ -0,0 +1,13 @@
+//
+//  BNRItemCell.h
+//  Homepwner
+//
+//  Created by Leksha Ramdenee on 2016-10-31.
+//  Copyright © 2016 Leksha Ramdenee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRItemCell : UITableViewCell
+
+@end
---
+++ Homepwner/BNRItemCell.m
@@ -0,0 +1,24 @@
+//
+//  BNRItemCell.m
+//  Homepwner
+//
+//  Created by Leksha Ramdenee on 2016-10-31.
+//  Copyright © 2016 Leksha Ramdenee. All rights reserved.
+//
+
+#import "BNRItemCell.h"
+
+@implementation BNRItemCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
