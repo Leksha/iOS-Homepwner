9b8e065378545aefb27fb3119fa578017f46c068
Leksha Ramdenee <leksha.ramdenee@tulip.io>
2016-11-07 11:41:45 -0500

    Set the height and width of the imageView on the itemCell to change programatically. Note: constraints can have outlets from the xib file to the file extension and be set programatically.

--- Homepwner/BNRItemCell.m
+++ Homepwner/BNRItemCell.m
@@ -8,6 +8,13 @@
 
 #import "BNRItemCell.h"
 
+@interface BNRItemCell ()
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageViewHeightConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageViewWidthConstraint;
+
+@end
+
 @implementation BNRItemCell
 
 #pragma mark Dynamic Type
@@ -17,6 +24,25 @@
     self.nameLabel.font = font;
     self.serialNumberLabel.font = font;
     self.valueLabel.font = font;
+    
+    // Programatically set the imageView size
+    static NSDictionary *imageSizeDictionary;
+    
+    if (!imageSizeDictionary){
+        imageSizeDictionary = @ { UIContentSizeCategoryExtraSmall : @40,
+                                  UIContentSizeCategorySmall      : @40,
+                                  UIContentSizeCategoryMedium     : @40,
+                                  UIContentSizeCategoryLarge      : @40,
+                                  UIContentSizeCategoryExtraLarge : @45,
+                                  UIContentSizeCategoryExtraExtraLarge :@55,
+                                  UIContentSizeCategoryExtraExtraExtraLarge : @65 };
+    }
+    
+    NSString *userSize = [[UIApplication sharedApplication] preferredContentSizeCategory];
+    NSNumber *imageSize = imageSizeDictionary[userSize];
+    self.imageViewHeightConstraint.constant = imageSize.floatValue;
+    self.imageViewWidthConstraint.constant = imageSize.floatValue;
+    
 }
 
 - (void)awakeFromNib {
