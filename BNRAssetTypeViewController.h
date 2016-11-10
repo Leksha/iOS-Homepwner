ee6a4d961021326906ecff108f036221d42bff18
Leksha Ramdenee <leksha.ramdenee@tulip.io>
2016-11-10 16:50:48 -0500

    Added BNRAssetTypeViewController to handler Asset Types

---
+++ BNRAssetTypeViewController.h
@@ -0,0 +1,16 @@
+//
+//  BNRAssetTypeViewController.h
+//  Homepwner
+//
+//  Created by Leksha Ramdenee on 2016-11-10.
+//  Copyright © 2016 Leksha Ramdenee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class BNRItem;
+
+@interface BNRAssetTypeViewController : UITableViewController
+
+@property (nonatomic, strong) BNRItem *item;
+
+@end
